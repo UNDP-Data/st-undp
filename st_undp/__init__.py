6d8b0540a40d043819f398674839a28888669d20
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2024-09-04 22:23:04 +0200

    Feat: add a styling function and basic font settings

---
+++ st_undp/__init__.py
@@ -0,0 +1,16 @@
+"""
+Styles and components for enhancing the look of Streamlit apps at UNDP.
+"""
+
+from importlib import resources
+
+import streamlit as st
+
+
+def apply_style():
+    """
+    Apply the CSS rules based on [UNDP Design System](https://design.undp.org).
+    """
+    with resources.open_text("st_undp", "style.html") as file:
+        body = file.read()
+    st.html(body)
