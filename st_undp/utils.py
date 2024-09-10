a7f406f629999fc77cf39607e57830ac694839ef
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2024-09-10 10:46:18 +0200

    Feat(utils): enable reading text and binary file resources

--- st_undp/utils.py
+++ st_undp/utils.py
@@ -3,9 +3,10 @@ Utility functions.
 """
 
 from importlib import resources
+from typing import Literal
 
 
-def read_file(file_path: str) -> str:
+def read_file(file_path: str, mode: Literal["r", "rb"] = "r") -> str | bytes:
     """
     Read a file resource as text.
 
@@ -13,12 +14,14 @@ def read_file(file_path: str) -> str:
     ----------
     file_path : str
         Relative path to the file from the package root.
+    mode : Literal["r", "rb"]
+        Mode can be 'r' or 'rb' to open as text or binary respectively.
 
     Returns
     -------
-    content : str
-        File content as string
+    content : str | bytes
+        File content as string or bytes.
     """
-    with resources.files("st_undp").joinpath(file_path).open("r") as file:
+    with resources.files("st_undp").joinpath(file_path).open(mode) as file:
         content = file.read()
     return content
