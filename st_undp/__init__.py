e7b49d677297f5ebcc53f5eb95336003bf9b3c00
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2024-09-11 18:47:10 +0200

    Feat(favicon): add a function to set page config with UNDP favicon

--- st_undp/__init__.py
+++ st_undp/__init__.py
@@ -2,6 +2,8 @@
 Styles and components for enhancing the look of Streamlit apps at UNDP.
 """
 
+from io import BytesIO
+
 import streamlit as st
 
 from .components import *
@@ -15,3 +17,14 @@ def apply_style():
     css = read_file("main.scss")
     body = f"<style>{css}</style>"
     st.html(body)
+
+
+def set_page_config(title: str, menu_items: dict):
+    image = read_file(f"images/favicon.ico", "rb")
+    st.set_page_config(
+        page_title=title,
+        page_icon=BytesIO(image),
+        layout="wide",
+        initial_sidebar_state="auto",
+        menu_items=menu_items,
+    )
