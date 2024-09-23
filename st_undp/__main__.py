6d8c4ec3269a2c9cae4b6b22a77dc8c0c878c4c3
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2024-09-23 12:01:37 +0200

    Feat(cli): add `--settings` option to include `startup.sh`

--- st_undp/__main__.py
+++ st_undp/__main__.py
@@ -47,9 +47,30 @@ def __modify_config():
 
 
 @cli.command()
-def configure():
+@click.option(
+    "--settings",
+    type=click.Choice(["style", "deployment"], case_sensitive=False),
+    default="style",
+    help="The type of configuration settings for your project. `style` modifies `config.yaml` to style your app"
+    " while `deployment` adds a `startup.sh` with commands to style and run your app on Azure App Service."
+    " See the package documentation for usage details.",
+)
+def configure(settings: str):
     """Configure the project settings."""
-    __modify_config()
+    if settings == "style":
+        __modify_config()
+    elif settings == "deployment":
+        file_name = "startup.sh"
+        script = read_file(file_name)
+        with open(file_name, "w") as file:
+            file.write(script)
+        click.echo(f"Added {file_name} for deployment to Azure Web Apps.")
+        message = click.style(
+            "Make sure to add 'startup.sh' as a startup command on Azure.",
+            bold=True,
+        )
+        click.echo(message)
+
 
 if __name__ == "__main__":
     cli()
---
+++ st_undp/startup.sh
@@ -0,0 +1,17 @@
+# This file is used to launch a Streamlit app on Azure App Service
+
+# 1. Add "startup.sh" as a "Startup Command" under "Stack settings"
+# in "Settings" -> "Configuration" for your Web App on Azure
+
+# 2. (Optional) Add commands to set up resources or download artefacts for
+# your app, e.g., a large data or model file
+# curl -o example.txt https://www.undp.org/robots.txt
+
+# 3. (Optional) Modify the name of the entry script below if needed
+python -m st_undp configure --settings style
+
+python -m streamlit run app.py \
+  --server.address 0.0.0.0 \
+  --server.port 80  \
+  --server.enableStaticServing true \
+  --browser.gatherUsageStats false
